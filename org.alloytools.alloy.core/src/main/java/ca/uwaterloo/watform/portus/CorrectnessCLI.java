1f5c26968c77630b73490d4bdcfbac6c7f7afe8b
Ryan Dancy <ryan@keal.ca>
2022-10-28 14:35:43 +0200

    Fix bug in correctness-checking with formula compatibility
    Turns out that A4Solution.eval() checks Sig/Field equality by pointer
    equality (i.e. == on the objects). Of course, since the A4Solution we
    generate was read back from XML, it won't have the same Sig/Field
    objects as the Alloy formula from the original command, causing
    exceptions when A4Solution.eval() doesn't recognize a Sig/Field object.
    To fix this, we go through and map all the Sig/Fields to the ones in the
    new A4Solution.

--- org.alloytools.alloy.core/src/main/java/ca/uwaterloo/watform/portus/CorrectnessCLI.java
+++ org.alloytools.alloy.core/src/main/java/ca/uwaterloo/watform/portus/CorrectnessCLI.java
@@ -1,10 +1,28 @@
 package ca.uwaterloo.watform.portus;
 
 import edu.mit.csail.sdg.alloy4.A4Reporter;
+import edu.mit.csail.sdg.alloy4.Err;
+import edu.mit.csail.sdg.alloy4.ErrorFatal;
 import edu.mit.csail.sdg.alloy4.XMLNode;
+import edu.mit.csail.sdg.ast.Assert;
 import edu.mit.csail.sdg.ast.Command;
+import edu.mit.csail.sdg.ast.Decl;
+import edu.mit.csail.sdg.ast.Expr;
+import edu.mit.csail.sdg.ast.ExprBinary;
+import edu.mit.csail.sdg.ast.ExprCall;
+import edu.mit.csail.sdg.ast.ExprConstant;
+import edu.mit.csail.sdg.ast.ExprHasName;
+import edu.mit.csail.sdg.ast.ExprITE;
+import edu.mit.csail.sdg.ast.ExprLet;
+import edu.mit.csail.sdg.ast.ExprList;
+import edu.mit.csail.sdg.ast.ExprQt;
+import edu.mit.csail.sdg.ast.ExprUnary;
+import edu.mit.csail.sdg.ast.ExprVar;
+import edu.mit.csail.sdg.ast.Func;
 import edu.mit.csail.sdg.ast.Module;
+import edu.mit.csail.sdg.ast.Sig;
 import edu.mit.csail.sdg.parser.CompUtil;
+import edu.mit.csail.sdg.parser.Macro;
 import edu.mit.csail.sdg.translator.A4Options;
 import edu.mit.csail.sdg.translator.A4Solution;
 import edu.mit.csail.sdg.translator.A4SolutionReader;
@@ -16,6 +34,7 @@ import java.io.StringReader;
 import java.io.StringWriter;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * A correctness checker CLI for Portus.
@@ -44,7 +63,127 @@ public final class CorrectnessCLI {
         return A4SolutionReader.read(null, new XMLNode(new StringReader(xml)));
     }
 
-    private static void processCommand(Module world, Command command, A4Options options) throws IOException {
+    /**
+     * When we convert a FortressSolution to an A4Solution via XML, A4SolutionWriter makes new Field/Sig
+     * objects, which means that A4Solution.eval() won't recognize them as the same as the Field/Sig
+     * objects in formulas meant for the old FortressSolution. So map any such objects to their equivalent values
+     * in the new solution, matching on names.
+     * Hopefully this doesn't mess anything up...
+     */
+    private static Expr mapFormulaToNewA4Solution(Expr formula, A4Solution solution) {
+        return new FortressVisitReturn<Expr>() {
+            @Override
+            public Expr visit(Sig sig) throws Err {
+                // find the sig in the solution with the same name - hope the label is a unique enough ID...
+                for (Sig solSig : solution.getAllReachableSigs()) {
+                    if (solSig.label.equals(sig.label)) {
+                        return solSig;
+                    }
+                }
+                throw new ErrorFatal("Could not find A4Solution match for sig: " + sig.label);
+            }
+
+            @Override
+            public Expr visit(Sig.Field field) throws Err {
+                // find the field in the solution with the same name + sig, hope this is unique enough...
+                for (Sig solSig : solution.getAllReachableSigs()) {
+                    if (solSig.label.equals(field.sig.label)) {
+                        for (Sig.Field sigField : solSig.getFields()) {
+                            if (sigField.label.equals(field.label)) {
+                                return sigField;
+                            }
+                        }
+                    }
+                }
+                throw new ErrorFatal("Could not find A4Solution match for field: " + field);
+            }
+
+            @Override
+            public Expr visit(ExprVar x) throws Err {
+                // It *should* be fine to not deal with ExprVars because in formulas they don't refer to atoms directly
+                return x;
+            }
+
+            @Override
+            public Expr visit(ExprBinary x) throws Err {
+                return x.op.make(x.pos, x.closingBracket, visitThis(x.left), visitThis(x.right));
+            }
+
+            @Override
+            public Expr visit(ExprList x) throws Err {
+                return ExprList.make(x.pos, x.closingBracket, x.op, x.args.stream()
+                        .map(this::visitThis)
+                        .collect(Collectors.toList()));
+            }
+
+            @Override
+            public Expr visit(ExprCall x) throws Err {
+                return ExprCall.make(x.pos, x.closingBracket, (Func) visitThis(x.fun), x.args.stream()
+                        .map(this::visitThis)
+                        .collect(Collectors.toList()), x.extraWeight);
+            }
+
+            @Override
+            public Expr visit(ExprConstant x) throws Err {
+                return x;
+            }
+
+            @Override
+            public Expr visit(ExprITE x) throws Err {
+                return ExprITE.make(x.pos, visitThis(x.cond), visitThis(x.left), visitThis(x.right));
+            }
+
+            @Override
+            public Expr visit(ExprLet x) throws Err {
+                return ExprLet.make(x.pos, (ExprVar) visitThis(x.var), visitThis(x.expr), visitThis(x.sub));
+            }
+
+            @Override
+            public Expr visit(ExprQt x) throws Err {
+                return x.op.make(x.pos, x.closingBracket, x.decls.stream()
+                        .map(this::visitDecl)
+                        .collect(Collectors.toList()), visitThis(x.sub));
+            }
+
+            @Override
+            public Expr visit(ExprUnary x) throws Err {
+                return x.op.make(x.pos, visitThis(x.sub));
+            }
+
+            @Override
+            public Expr visit(Func x) throws Err {
+                // TODO: this might recurse infinitely on recursive calls (but we don't support this anyways yet...)
+                return new Func(x.pos, x.labelPos, x.label, x.decls.stream()
+                        .map(this::visitDecl)
+                        .collect(Collectors.toList()), visitThis(x.returnDecl), visitThis(x.getBody()));
+            }
+
+            private Decl visitDecl(Decl decl) {
+                return new Decl(decl.isPrivate, decl.disjoint, decl.disjoint2, decl.isVar,
+                    decl.names.stream()
+                            .map(name -> (ExprHasName) visitThis(name))
+                            .collect(Collectors.toList()),
+                    visitThis(decl.expr));
+            }
+
+            @Override
+            public Expr visit(ExprElementOf x) throws Err {
+                return ExprElementOf.make(x.tuple, visitThis(x.sub));
+            }
+
+            @Override
+            public Expr visit(Assert x) throws Err {
+                throw new ErrorFatal("We don't support Assert in formulas!");
+            }
+
+            @Override
+            public Expr visit(Macro macro) throws Err {
+                throw new ErrorFatal("We don't support Macro in formulas!");
+            }
+        }.visitThis(formula);
+    }
+
+    private static void processCommand(Module world, Command command, A4Options options) {
         System.out.println("  Command: " + command.label);
 
         try {
@@ -71,9 +210,14 @@ public final class CorrectnessCLI {
 
             System.out.println("  Kodkod interpretation: " + kodkodSol);
 
+            // The Kodkod-converted formula uses different objects for Sig/Field than the original formula (because it
+            // was reconstructed from XML), so A4Solution.eval() won't recognize them as equivalent. Fix this by
+            // mapping the Sig/Field objects to those in the new A4Solution.
+            Expr kodkodCompatibleFormula = mapFormulaToNewA4Solution(command.formula, kodkodSol);
+
             // The assertion in the command needs to be valid according to Kodkod too
             // Typechecking should ensure we don't get any class cast errors here...
-            boolean assertionValid = (boolean) kodkodSol.eval(command.formula);
+            boolean assertionValid = (boolean) kodkodSol.eval(kodkodCompatibleFormula);
             if (assertionValid) {
                 System.out.println("  OK");
             } else {
