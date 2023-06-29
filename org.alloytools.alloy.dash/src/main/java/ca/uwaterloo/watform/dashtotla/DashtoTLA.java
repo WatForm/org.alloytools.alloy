ebdda021644acdb56c7a8a12af6c7a33d2d47a48
Mathew K J <mathewkj2048@gmail.com>
2023-06-29 00:42:31 -0400

    '

--- org.alloytools.alloy.dash/src/main/java/ca/uwaterloo/watform/dashtotla/DashtoTLA.java
+++ org.alloytools.alloy.dash/src/main/java/ca/uwaterloo/watform/dashtotla/DashtoTLA.java
@@ -97,7 +97,7 @@ public class DashtoTLA
             String srcState = d.getTransSrc(s).toString();
             String CONF = "\n\t/\\"+isInState(srcState);
 
-            // formula for conf' //t his a test of script
+            // formula for conf' //t his a test of scr
             List<String> ENTER = toStringList(d.entered(s));
             List<String> EXIT = toStringList(d.exited(s));
             List<String> EXITresolved = new ArrayList<>();
