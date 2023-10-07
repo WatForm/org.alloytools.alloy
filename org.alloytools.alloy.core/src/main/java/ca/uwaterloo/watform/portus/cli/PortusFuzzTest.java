f821bb19f212408ef768a2bf820d7cfa704f5a84
Ryan Dancy <ryan@keal.ca>
2023-10-07 21:25:32 +0200

    Change fuzz test to consume string as Alloy model

--- org.alloytools.alloy.core/src/main/java/ca/uwaterloo/watform/portus/cli/PortusFuzzTest.java
+++ org.alloytools.alloy.core/src/main/java/ca/uwaterloo/watform/portus/cli/PortusFuzzTest.java
@@ -1,19 +1,36 @@
 package ca.uwaterloo.watform.portus.cli;
 
+import com.code_intelligence.jazzer.api.FuzzedDataProvider;
+import edu.mit.csail.sdg.alloy4.ErrorSyntax;
 import edu.mit.csail.sdg.ast.Command;
+import edu.mit.csail.sdg.ast.Module;
 import edu.mit.csail.sdg.ast.Sig;
+import edu.mit.csail.sdg.parser.CompUtil;
 import edu.mit.csail.sdg.translator.A4Options;
 
+@SuppressWarnings("unused")
 public class PortusFuzzTest {
 
-    public void autofuzz(Iterable<Sig> sigs, Command command, A4Options options) {
-        if (sigs == null || command == null || options == null) {
-            return;
-        }
-        CorrectnessChecker correctness = new CorrectnessChecker();
-        CorrectnessChecker.Result result = correctness.checkCorrectness(sigs, command, options);
-        if (result.kind != CorrectnessChecker.Result.Kind.OK) {
-            throw new RuntimeException();
+    public static void fuzzerTestOneInput(FuzzedDataProvider data) {
+        String alloy = data.consumeRemainingAsAsciiString();
+        try {
+            Module world = CompUtil.parseEverything_fromString(null, alloy);
+            if (world.getAllCommands().isEmpty()) {
+                return;
+            }
+
+            CorrectnessChecker correctness = new CorrectnessChecker();
+            Iterable<Sig> sigs = world.getAllReachableSigs();
+            Command command = world.getAllCommands().get(0);
+            A4Options options = new A4Options();
+
+            CorrectnessChecker.Result result = correctness.checkCorrectness(sigs, command, options);
+            if (result.kind == CorrectnessChecker.Result.Kind.KODKOD_SAT_FORTRESS_UNSAT
+                || result.kind == CorrectnessChecker.Result.Kind.FORTRESS_INTERPRETATION_INVALID) {
+                throw new RuntimeException();
+            }
+        } catch (ErrorSyntax syntax) {
+            // not an error for fuzz testing
         }
     }
 
