9dee109e953c4d8097cb152d2b43d0b737953538
Ryan Dancy <ryan@keal.ca>
2023-10-07 20:21:12 +0200

    Make fuzz test null-safe

--- org.alloytools.alloy.core/src/main/java/ca/uwaterloo/watform/portus/cli/PortusFuzzTest.java
+++ org.alloytools.alloy.core/src/main/java/ca/uwaterloo/watform/portus/cli/PortusFuzzTest.java
@@ -7,6 +7,9 @@ import edu.mit.csail.sdg.translator.A4Options;
 public class PortusFuzzTest {
 
     public void autofuzz(Iterable<Sig> sigs, Command command, A4Options options) {
+        if (sigs == null || command == null || options == null) {
+            return;
+        }
         CorrectnessChecker correctness = new CorrectnessChecker();
         CorrectnessChecker.Result result = correctness.checkCorrectness(sigs, command, options);
         if (result.kind != CorrectnessChecker.Result.Kind.OK) {
