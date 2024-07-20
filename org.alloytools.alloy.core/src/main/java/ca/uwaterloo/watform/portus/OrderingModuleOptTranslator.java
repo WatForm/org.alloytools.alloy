e77e7eb96e765899f364a7eda7d79c7510291a1b
Ryan Dancy <ryan@keal.ca>
2024-07-20 19:17:57 -0400

    Fix edge case where ordered sig has scope set to 1

--- org.alloytools.alloy.core/src/main/java/ca/uwaterloo/watform/portus/OrderingModuleOptTranslator.java
+++ org.alloytools.alloy.core/src/main/java/ca/uwaterloo/watform/portus/OrderingModuleOptTranslator.java
@@ -193,7 +193,14 @@ final class OrderingModuleOptTranslator extends AbstractTranslator implements Sc
                 }
 
                 Var inputVar = Term.mkVar(nameGenerator.freshName("x"));
-                Term lookupTableTerm = PortusUtil.mkExhaustiveLookupTable(inputVar, lookupTable);
+                Term lookupTableTerm;
+                if (lookupTable.isEmpty()) {
+                    // The ordered sig has a scope of 1, so every value of next is ignored.
+                    // This is an edge case so don't bother optimizing specially - assign it arbitrarily.
+                    lookupTableTerm = Term.mkDomainElement(deRange.a, sort);
+                } else {
+                    lookupTableTerm = PortusUtil.mkExhaustiveLookupTable(inputVar, lookupTable);
+                }
                 FunctionDefinition definition = FunctionDefinition.mkFunctionDefinition(
                         nextFuncName, Collections.singletonList(inputVar.of(sort)), sort, lookupTableTerm);
                 context.addFunctionDefinition(definition);
