64013f8d4e8a6ea9b84b3a57c7590778b36765af
Ryan Dancy <ryan@keal.ca>
2025-09-14 15:47:10 +0100

    Fill in missing functions on return from Z3
    On occasion Z3 will fail to return a function when it's been completely
    optimized out internally to Z3. This might occur when e.g. we quantify
    over a sort of size 0. For these edge cases it doesn't matter what the
    function's value is, so we fill it in arbitrarily.

--- org.alloytools.alloy.core/src/main/java/ca/uwaterloo/watform/portus/SolutionFinder.java
+++ org.alloytools.alloy.core/src/main/java/ca/uwaterloo/watform/portus/SolutionFinder.java
@@ -10,14 +10,11 @@ import fortress.interpretation.Interpretation;
 import fortress.modelfinders.ErrorResult;
 import fortress.modelfinders.ModelFinder;
 import fortress.modelfinders.ModelFinderResult;
-import fortress.msfol.IntegerLiteral;
-import fortress.msfol.Sort;
-import fortress.msfol.Value;
+import fortress.msfol.*;
 import scala.collection.immutable.Seq;
+import scala.jdk.javaapi.CollectionConverters;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -97,12 +94,37 @@ class SolutionFinder implements AutoCloseable {
             sortInterpretationsScala.put(sort, PortusUtil.toScalaSeq(sortInterpretations.get(sort)));
         }
 
+        // If any function declarations are missing, add them back as arbitrary
+        // Z3 sometimes doesn't return functions when they're completely optimized out
+        Set<FunctionDefinition> newFuncDefs = new HashSet<>();
+        translated.getTheory().functionDeclarations().foreach(funcDecl -> {
+            if (interpretation.functionDefinitions().find(def -> def.name().equals(funcDecl.name())).isEmpty()) {
+                List<AnnotatedVar> args = new ArrayList<>();
+                for (int i = 0; i < funcDecl.argSorts().size(); i++) {
+                    args.add(AnnotatedVar.apply(Term.mkVar("x" + i), funcDecl.argSorts().apply(i)));
+                }
+                Term body;
+                if (funcDecl.resultSort().equals(Sort.Bool())) {
+                    body = Term.mkBottom();
+                } else {
+                    body = Term.mkDomainElement(1, funcDecl.resultSort());
+                }
+                FunctionDefinition funcDef = FunctionDefinition.mkFunctionDefinition(
+                        funcDecl.name(), args, funcDecl.resultSort(), body);
+                newFuncDefs.add(funcDef);
+            }
+            return null;
+        });
+        scala.collection.immutable.Set<FunctionDefinition> newFuncDefsSet = CollectionConverters.asScala(newFuncDefs)
+                .<FunctionDefinition>toSet();
+
         // Add the function definitions from the theory because they aren't returned from Fortress
         //noinspection unchecked
         return BasicInterpretation$.MODULE$.apply(
                 PortusUtil.toScalaMap(sortInterpretationsScala),
                 interpretation.constantInterpretations(),
-                interpretation.functionDefinitions().concat(translated.getTheory().functionDefinitions()).toSet());
+                interpretation.functionDefinitions().concat(translated.getTheory().functionDefinitions())
+                        .concat(newFuncDefsSet).toSet());
     }
 
     @Override
